2e8d3cfd53d4771f15bb928f47fdfc0be1e34c4c
pc <pc@LAPTOP-6RJ0ND3N>
2023-03-01 18:48:09 -0500

    Agregar archivos de proyecto.

---
+++ PañaleraGenesis/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace PañaleraGenesis
+{
+    partial class Frmlogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.btningresar = new System.Windows.Forms.Button();
+            this.txtcontrasena = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.Color.LightCyan;
+            this.panel1.Location = new System.Drawing.Point(158, 44);
+            this.panel1.Margin = new System.Windows.Forms.Padding(10, 8, 10, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(617, 233);
+            this.panel1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe Script", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label5.Location = new System.Drawing.Point(142, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(265, 34);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Variedades y Pañalera";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ravie", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.LightPink;
+            this.label2.Location = new System.Drawing.Point(91, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(363, 86);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Génesis";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(31, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(529, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sistema de Control de Inventario";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel2.Controls.Add(this.btnlimpiar);
+            this.panel2.Controls.Add(this.btnsalir);
+            this.panel2.Controls.Add(this.btningresar);
+            this.panel2.Controls.Add(this.txtcontrasena);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.txtusuario);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(158, 288);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(617, 276);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnlimpiar.ForeColor = System.Drawing.Color.Black;
+            this.btnlimpiar.Location = new System.Drawing.Point(449, 141);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(135, 42);
+            this.btnlimpiar.TabIndex = 6;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnsalir.ForeColor = System.Drawing.Color.Black;
+            this.btnsalir.Location = new System.Drawing.Point(478, 212);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(106, 47);
+            this.btnsalir.TabIndex = 5;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            // 
+            // btningresar
+            // 
+            this.btningresar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btningresar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btningresar.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btningresar.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.btningresar.Location = new System.Drawing.Point(449, 27);
+            this.btningresar.Name = "btningresar";
+            this.btningresar.Size = new System.Drawing.Size(135, 51);
+            this.btningresar.TabIndex = 4;
+            this.btningresar.Text = "Ingresar";
+            this.btningresar.UseVisualStyleBackColor = false;
+            this.btningresar.Click += new System.EventHandler(this.btningresar_Click);
+            // 
+            // txtcontrasena
+            // 
+            this.txtcontrasena.Location = new System.Drawing.Point(178, 141);
+            this.txtcontrasena.Name = "txtcontrasena";
+            this.txtcontrasena.PasswordChar = '*';
+            this.txtcontrasena.Size = new System.Drawing.Size(229, 61);
+            this.txtcontrasena.TabIndex = 3;
+            this.txtcontrasena.TextChanged += new System.EventHandler(this.txtcontrasena_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(3, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 33);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Contraseña";
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(178, 24);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(229, 61);
+            this.txtusuario.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(10, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 33);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Usuario";
+            // 
+            // Frmlogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(34F, 54F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 604);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Ravie", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Pink;
+            this.Margin = new System.Windows.Forms.Padding(14, 11, 14, 11);
+            this.Name = "Frmlogin";
+            this.Text = "ventana1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Panel panel2;
+        private TextBox txtcontrasena;
+        private Label label4;
+        private TextBox txtusuario;
+        private Label label3;
+        private Button btnsalir;
+        private Button btningresar;
+        private Button btnlimpiar;
+        private Label label5;
+    }
+}
---
+++ PañaleraGenesis/Form1.cs
@@ -0,0 +1,51 @@
+namespace PañaleraGenesis
+{
+    public partial class Frmlogin : Form
+    {
+        public Frmlogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btningresar_Click(object sender, EventArgs e) 
+        {   String usuario, contrasena;
+
+            usuario= txtusuario.Text;
+            contrasena=txtcontrasena.Text;
+
+            if (usuario == "usuario C#" && contrasena == "123")
+                ¨{ }
+                Frminicio.show()
+                
+        
+            end if
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtcontrasena_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
